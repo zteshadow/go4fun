9a6e9465163336fdb90bf59ffe85ab57d78bbc8f
Samuel Song <samuel.song@agoda.com>
2024-10-18 08:48:17 +0700

    init rust

---
+++ rust/learn/readme.md
@@ -0,0 +1,11 @@
+
+# Install
+```bash
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source ~/.cargo/env.fish
+```
+
+# Book
+```bash
+rustup doc --book 
+```
--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,3 +1,10 @@
-import bar from "./bar";
 
-bar();
+import _ from 'lodash';
+
+function component() {
+    const element = document.createElement('div');
+    element.innerHTML = _.join(['Hello webpack']);
+    return element;
+}
+
+document.body.appendChild(component());
